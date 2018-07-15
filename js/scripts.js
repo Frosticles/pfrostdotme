2e439e09ec3cc0094ca8f6d4d918040808f61ff8
Peter Frost <frost@Peters-MacBook-Pro.local>
2018-07-15 12:03:41 +0100

    Added more hero sections Added contact page Added hero seperators

--- js/scripts.js
+++ js/scripts.js
@@ -36,4 +36,15 @@ $(document).ready(function() {
 			.appendTo('#slideshow3');
 		}
 	},  (4000 + (Math.random()*2000)));
+	
+	setInterval(function() { 
+		if (document.hasFocus()) {
+		  $('#slideshow4 > div:first')
+			.fadeOut(1000)
+			.next()
+			.fadeIn(1000)
+			.end()
+			.appendTo('#slideshow4');
+		}
+	},  (4000 + (Math.random()*2000)));
 });
