2a63109231291e1e4893a8a46b28e36947ed0c24
Peter Frost <frost@iPhone-Katja.idealguest>
2018-08-19 23:18:02 +0200

    Add Brotli pre-processed files

--- .preprocess.sh
+++ .preprocess.sh
@@ -2,10 +2,11 @@
 # npm install uglify-js -g
 # npm install csso-cli -g
 # brew install zopfli
+# brew install brotli
 # brew install rename
 
 # Cleanup from previous (possibly incomplete) runs
-find ./ \( -name "*.min.gz" -o -name "*.min" -o -name "*.gz" \) | xargs rm 
+find ./ \( -name "*.min.gz" -o -name "*.min" -o -name "*.gz" -o -name "*.br" \) | xargs rm 
 
 find ./ -name "*.html" | xargs -I % html-minifier --collapse-whitespace --remove-comments --remove-optional-tags \
                              --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace \
@@ -17,8 +18,14 @@ find ./ -name "*.js" | xargs -I % uglifyjs --compress --mangle --verbose -o %.mi
 
 find ./ \( -name "*.min" -o -name "*.svg" -o -name "*.txt" -o -name "*.xml" -o -name "*.csv" \
            -o -name "*.json" -o -name "*.bmp" -o -name "*.otf" -o -name "*.ttf" \
+           -o -name "*.webmanifest" \) | xargs -I % brotli --verbose --best %
+
+find ./ \( -name "*.min" -o -name "*.svg" -o -name "*.txt" -o -name "*.xml" -o -name "*.csv" \
+           -o -name "*.json" -o -name "*.bmp" -o -name "*.otf" -o -name "*.ttf" \
            -o -name "*.webmanifest" \) | xargs -I % zopfli -v --i15 %
 
 find ./ -name "*.min.gz" | xargs -I % rename -v 's/\.min.gz$/.gz/' %
+
+find ./ -name "*.min.br" | xargs -I % rename -v 's/\.min.br$/.br/' %
 
 find ./ -name "*.min" | xargs rm
