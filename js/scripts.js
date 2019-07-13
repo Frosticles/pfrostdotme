9252cdb2e678bc61e86c270d14f79e3f4b807758
Peter Frost <frosticless@hotmail.com>
2019-07-13 18:54:12 +0100

    Add random splash text

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,58 @@ custom: {
 	}
 });
 
+var lines = {
+	"What... is the capital of Assyria?",
+	"\'Tis but a scratch!",
+	"some farcical aquatic ceremony.",
+	"Right. One... two... five!",
+	"It's just a flesh wound.",
+	"Ni!",
+	"Help! Help! I'm being repressed!",
+	"We're an anarcho-syndicalist commune",
+	"A scratch? Your arm's off!",
+	"You've got no arms left!",
+	"What have the Romans ever done for us?",
+	"Yippee-ki-yay.",
+	"Life was like a box of chocolates.",
+	"Run, Forrest! Run!",
+	"Pining for the fjords",
+	"This... is an ex parrot",
+	"They'll never take our freedom!",
+	"Are you not entertained‽",
+	"It's one louder than 10",
+	"The Royale with cheese.",
+	"Wax on, wax off.",
+	"hello world",
+	"0xDEADBEEF",
+	"Good morning, Vietnam!",
+	"My precious.",
+	"It's alive!",
+	"Go ahead, make my day.",
+	"Do I feel lucky?",
+	"I'm having an old friend for dinner.",
+	"Shaken, not stirred.",
+	"We don't need roads.",
+	"Say hello to my little friend!",
+	"And don't call me Shirley.",
+	"Here's Johnny!",
+	"Houston, we have a problem.",
+	"You can't handle the truth!",
+	"I'll be back.",
+	"Why... so... serious?"
+	"I am your father.",
+	"You don't talk about Fight Club.",
+	"You're gonna need a bigger boat.",
+	"Good luck, we're all counting on you.",
+	"foobar",
+	"0xA5A5A5A5",
+	"undefined reference",
+	"#define + -",
+	"precise bus fault",
+	"for (char i = 0; i < 256; i++) {}"
+}
+
+
 $(document).ready(function() {
 
 	/*
@@ -33,7 +85,7 @@ $(document).ready(function() {
 	*/
 
 	var jumboCount = 0;
-	var jumboTxt = 'hello world ';
+	var jumboTxt = lines[random(lines.length)];
 	var typeSpeed = 80;
 	var spinSpeed = 80;
 
