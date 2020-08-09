396f5f937cbf50c7ce05f6d381595dd420990ddf
Peter Frost <frosticless@hotmail.com>
2020-08-09 23:30:28 +0100

    Refactoring, rewrote typewriter code to be much more succinct

--- js/scripts.js
+++ js/scripts.js
@@ -18,299 +18,285 @@ else
 }
 
 
-var lines = [
-	"What... is the capital of Assyria?",
-	"'Tis but a scratch!",
-	"some farcical aquatic ceremony.",
-	"Right. One... two... five!",
-	"It's just a flesh wound.",
-	"Ni!",
-	"Help! Help! I'm being repressed!",
-	"We're an anarcho-syndicalist commune",
-	"A scratch? Your arm's off!",
-	"You've got no arms left!",
-	"What have the Romans ever done for us?",
-	"Yippee-ki-yay.",
-	"Life is like a box of chocolates.",
-	"Run, Forrest! Run!",
-	"Pining for the fjords",
-	"This... is an ex parrot",
-	"They'll never take our freedom!",
-	"Are you not entertained‽",
-	"It's one louder than 10",
-	"The Royale with cheese.",
-	"Wax on, wax off.",
-	"hello world",
-	"0xDEADBEEF",
-	"Good morning Vietnam!",
-	"My precious.",
-	"It's alive!",
-	"Go ahead, make my day.",
-	"Do I feel lucky?",
-	"I'm having an old friend for dinner.",
-	"Shaken, not stirred.",
-	"We don't need roads.",
-	"Say hello to my little friend!",
-	"And don't call me Shirley.",
-	"Here's Johnny!",
-	"Houston, we have a problem.",
-	"You can't handle the truth!",
-	"I'll be back.",
-	"Why... so... serious?",
-	"I am your father.",
-	"You don't talk about Fight Club.",
-	"You're gonna need a bigger boat.",
-	"Good luck, we're all counting on you.",
-	"foobar",
-	"0xA5A5A5A5",
-	"undefined reference",
-	"#define + -",
-	"precise bus fault",
-	"imprecise bus fault",
-	"for (uint8_t i = 0; i < 256; i++) {}",
-	"you can't fight in here, this is the war room!",
-	"0x8BADF00D",
-	"Sometimes, I dream about cheese"
-];
 
-var frames = [
-	'(•_•)', 10,
-	'(\u0701•_•)', 12,
-	'(\u0701•_•)>⌐◾-◾', 13,
-	'(\u0701•⌐)>-◾', 14,
-	'(\u0701◾_◾)', 15,
-	'(◾_◾)', 25
-];
 
-let readyFunction = function(){
 
-	/*
-		Hamburger button menu trigger
-	*/
-
-	var hamburger = document.querySelector(".hamburger");
-	var sideMenu = document.querySelector(".sidenav");
-				
-	hamburger.addEventListener("click", function() {
-		sideMenu.classList.toggle("is-active");  
-		hamburger.classList.toggle("is-active");
-		
-		if (hamburger.classList.contains("is-active")) 
-		{
-			document.getElementById("menu-hamburger").style.transform = "translate3d(250px, 0, 0)";
-
-		}
-		else 
-		{
-			document.getElementById("menu-hamburger").style.transform = "translate3d(0px, 0, 0)";
-		}
-	});
-
-	/*
-		Jumbotron typewriter code 
-	*/
-
-	var jumboCount = 0;
-	var jumboTxt = lines[Math.floor(Math.random() * lines.length)];
-	var typeSpeed = 80;
-	var spinSpeed = 80;
-
-	jumboTxt += " "
-
-	if (document.getElementById("jumbotron")) {
-		if (document.getElementById("jumbotron").title.length > 0)
-		{
-			jumboTxt = document.getElementById("jumbotron").title
-		}
-		setTimeout(spinner0, 500);
-	}
-		
-	function typeWriter() {
-		if (jumboCount < jumboTxt.length) {
-			document.getElementById("jumbotron").innerHTML += jumboTxt.charAt(jumboCount);
-		}
-
-		if (jumboCount < jumboTxt.length) {
-			jumboCount++;
-			setTimeout(typeWriter, typeSpeed);
-		}
-	}
-		
-	function cursorAdd() {
-		document.getElementById("jumbotron").innerHTML = document.getElementById("jumbotron").innerHTML.substring(0, document.getElementById("jumbotron").innerHTML.length);
-		document.getElementById("jumbotron").innerHTML += "█";
-		setTimeout(cursorDelete, 500);
-	}
-	function cursorDelete() {
-		document.getElementById("jumbotron").innerHTML = document.getElementById("jumbotron").innerHTML.replace("█", " ");
-		setTimeout(cursorAdd, 500);
-	}
-		
-	function spinner0() {
-		if (jumboCount < 4){
-			document.getElementById("jumbotron").innerHTML = '|';
-			setTimeout(spinner1, spinSpeed);
-			jumboCount++;
-		} else {
-			jumboCount = 0;
-			document.getElementById("jumbotron").innerHTML = '';
-			typeWriter();
-		}
-	}
-		
-	function spinner1() {
-		document.getElementById("jumbotron").innerHTML = '/';
-		setTimeout(spinner2, spinSpeed);
-	}
-	function spinner2() {
-		document.getElementById("jumbotron").innerHTML = '—';
-		setTimeout(spinner3, spinSpeed);
-	}
-	function spinner3() {
-		document.getElementById("jumbotron").innerHTML = '\\';
-		setTimeout(spinner0, spinSpeed);
-	}
-
-	/*
-		Document title typewriter code 
-	*/
-
-	var titleCount = 0;
-	var titleTxt = 'Peter Frost';
-	setTimeout(titleSpinner, 500);
-		
-	function titleTypeWriter() {
-		if (titleCount < titleTxt.length) {
-			var stringToAdd = titleTxt.charAt(titleCount);
-
-			if (stringToAdd == ' ') {
-				stringToAdd += titleTxt.charAt(titleCount + 1);
-				titleTxt = titleTxt.slice(0, titleCount) + titleTxt.slice(titleCount + 1);
-			}
-			document.title += stringToAdd;
-		}
-
-		if (titleCount < titleTxt.length) {
-			titleCount++;
-			setTimeout(titleTypeWriter, typeSpeed);
-		}
-	}
-
-	function titleSpinner() {
-		if (titleCount < 3){
-			document.title += '.';
-			setTimeout(titleSpinner, 500);
-			titleCount++;
-		} else {
-			titleCount = 0;
-			document.title = '';
-			titleTypeWriter();
-		}
-	}
-
-	/*
-		Slideshow functions for all the hero images
-	*/
-
-	function transitionFrame(frames)
+class common {
+	readyFunction() 
 	{
-		for (let i = 0; i < frames.length; i++) 
-		{
-			if (frames[i].style.opacity != 0)
-			{
-				let nextFrame = (i == (frames.length - 1)) ? 0 : (i + 1);
-				frames[i].style.opacity = 0;
-				frames[nextFrame].style.display = 'inline';
-				
-				// The timeout is to fix a really annoying bug where if the display
-				// style changes at the same time as the opacity, the css animation
-				// won't occur, and the image will just pop in.
-				setTimeout(() => { frames[nextFrame].style.opacity = 1; }, 100);
-				return;
-			}
-		}
-		console.log("Hmm, didn't find a frame in there with a non-zero opacity: " + frames);
+		this.initTypewritter();
+		this.initHamburger();
+		this.initSlideshow();
+		this.initAddressBarAnimation();
 	}
 
-	if (document.getElementById("slideshow")) {
+	initAddressBarAnimation()
+	{
+		var frameIndex = 0
+		var frameCount = 0
+		var advanceRight = true
+		var countUp = true
+		const animationFrames = [
+			'(•_•)', 10,
+			'(\u0701•_•)', 12,
+			'(\u0701•_•)>⌐◾-◾', 13,
+			'(\u0701•⌐)>-◾', 14,
+			'(\u0701◾_◾)', 15,
+			'(◾_◾)', 25
+		];
 
-		setInterval(function() { 
-			if (document.hasFocus()) 
+		setInterval(function() 
+		{ 
+			let frameUpdate = (decodeURI(window.location.hash) != ('#' + animationFrames[frameIndex]))
+			
+			if (frameUpdate == true)
 			{
-				transitionFrame(document.getElementById("slideshow").querySelectorAll("div"));
+				window.location.replace('#' + animationFrames[frameIndex])
 			}
-		},  (4000 + (Math.random()*2000)));
 
-		setInterval(function() { 
-			if (document.hasFocus()) 
-			{
-				transitionFrame(document.getElementById("slideshow2").querySelectorAll("div"));
+			
+			if (frameIndex >= (animationFrames.length - 2)) {
+				advanceRight = false;
+			} else if (frameIndex <= 0) {
+				advanceRight = true;
 			}
-		},  (4000 + (Math.random()*2000)));
 
-		setInterval(function() { 
-			if (document.hasFocus()) 
-			{
-				transitionFrame(document.getElementById("slideshow3").querySelectorAll("div"));
+			if (frameCount >= animationFrames[(animationFrames.length - 1)]) {
+				countUp = false;
+			} else if (frameCount <= 0) {
+				countUp = true;
 			}
-		},  (4000 + (Math.random()*2000)));
 
-		setInterval(function() { 
-			if (document.hasFocus()) 
-			{
-				transitionFrame(document.getElementById("slideshow4").querySelectorAll("div"));
+			if ((frameCount >= animationFrames[frameIndex + 1]) && (advanceRight == true)) {
+				frameIndex += 2
+			} else if ((frameCount <= animationFrames[frameIndex - 1]) && (advanceRight == false)) {
+				frameIndex -= 2
 			}
-		},  (4000 + (Math.random()*2000)));
-		
+
+			frameCount += countUp ? 1 : -1
+		}, 100);
 	}
 
-	/*
-		Code to animate address bar
-	*/
-
-	var frameIndex = 0
-	var frameCount = 0
-	var advanceRight = true
-	var countUp = true
-
-	setInterval(function() 
-	{ 
-		let frameUpdate = (decodeURI(window.location.hash) != ('#' + frames[frameIndex]))
-		
-		if (frameUpdate == true)
+	initSlideshow()
+	{
+		function transitionPicture(picture)
 		{
-			window.location.replace('#' + frames[frameIndex])
+			for (let i = 0; i < picture.length; i++) 
+			{
+				if (picture[i].style.opacity != 0)
+				{
+					let nextPicture = (i == (picture.length - 1)) ? 0 : (i + 1);
+					picture[i].style.opacity = 0;
+					picture[nextPicture].style.display = 'inline';
+					
+					// The timeout is to fix a really annoying bug where if the display
+					// style changes at the same time as the opacity, the css animation
+					// won't occur, and the image will just pop in.
+					setTimeout(() => { picture[nextPicture].style.opacity = 1; }, 100);
+					return;
+				}
+			}
+			console.log("Hmm, didn't find a frame in there with a non-zero opacity: " + picture);
 		}
 
-		
-		if (frameIndex >= (frames.length - 2)) {
-			advanceRight = false;
-		} else if (frameIndex <= 0) {
-			advanceRight = true;
+		if (document.getElementById("slideshow")) 
+		{
+
+			setInterval(function() 
+			{ 
+				if (document.hasFocus()) 
+				{
+					transitionPicture(document.getElementById("slideshow").querySelectorAll("div"));
+				}
+			}, (4000 + (Math.random()*2000)));
+
+			setInterval(function() 
+			{ 
+				if (document.hasFocus()) 
+				{
+					transitionPicture(document.getElementById("slideshow2").querySelectorAll("div"));
+				}
+			}, (4000 + (Math.random()*2000)));
+
+			setInterval(function() 
+			{ 
+				if (document.hasFocus()) 
+				{
+					transitionPicture(document.getElementById("slideshow3").querySelectorAll("div"));
+				}
+			}, (4000 + (Math.random()*2000)));
+
+			setInterval(function() 
+			{ 
+				if (document.hasFocus()) 
+				{
+					transitionPicture(document.getElementById("slideshow4").querySelectorAll("div"));
+				}
+			}, (4000 + (Math.random()*2000)));
+		}
+	}
+
+	initHamburger()
+	{
+		var hamburger = document.querySelector(".hamburger");
+		var sideMenu = document.querySelector(".sidenav");
+					
+		hamburger.addEventListener("click", function() 
+		{
+			sideMenu.classList.toggle("is-active");  
+			hamburger.classList.toggle("is-active");
+			
+			if (hamburger.classList.contains("is-active")) 
+			{
+				document.getElementById("menu-hamburger").style.transform = "translate3d(250px, 0, 0)";
+
+			}
+			else 
+			{
+				document.getElementById("menu-hamburger").style.transform = "translate3d(0px, 0, 0)";
+			}
+		});
+	}
+
+	initTypewritter()
+	{
+		const lines = [
+			"What... is the capital of Assyria?",
+			"'Tis but a scratch!",
+			"some farcical aquatic ceremony.",
+			"Right. One... two... five!",
+			"It's just a flesh wound.",
+			"Ni!",
+			"Help! Help! I'm being repressed!",
+			"We're an anarcho-syndicalist commune",
+			"A scratch? Your arm's off!",
+			"You've got no arms left!",
+			"What have the Romans ever done for us?",
+			"Yippee-ki-yay.",
+			"Life is like a box of chocolates.",
+			"Run, Forrest! Run!",
+			"Pining for the fjords",
+			"This... is an ex parrot",
+			"They'll never take our freedom!",
+			"Are you not entertained‽",
+			"It's one louder than 10",
+			"The Royale with cheese.",
+			"Wax on, wax off.",
+			"hello world",
+			"0xDEADBEEF",
+			"Good morning Vietnam!",
+			"My precious.",
+			"It's alive!",
+			"Go ahead, make my day.",
+			"Do I feel lucky?",
+			"I'm having an old friend for dinner.",
+			"Shaken, not stirred.",
+			"We don't need roads.",
+			"Say hello to my little friend!",
+			"And don't call me Shirley.",
+			"Here's Johnny!",
+			"Houston, we have a problem.",
+			"You can't handle the truth!",
+			"I'll be back.",
+			"Why... so... serious?",
+			"I am your father.",
+			"You don't talk about Fight Club.",
+			"You're gonna need a bigger boat.",
+			"Good luck, we're all counting on you.",
+			"foobar",
+			"0xA5A5A5A5",
+			"undefined reference",
+			"#define + -",
+			"precise bus fault",
+			"imprecise bus fault",
+			"for (uint8_t i = 0; i < 256; i++) {}",
+			"you can't fight in here, this is the war room!",
+			"0x8BADF00D",
+			"Sometimes, I dream about cheese"
+		];
+		const titleTxt = 'Peter Frost';
+		const numSpins = 4;
+		var frameCount = 0;
+		var jumboTxt = lines[Math.floor(Math.random() * lines.length)] + " ";
+		var typeInverval = 80;
+		var numSpaces = 0;
+		var jumbotronElement = document.getElementById("jumbotron");
+
+		if (jumbotronElement) 
+		{
+			if (jumbotronElement.title.length > 0)
+			{
+				jumboTxt = jumbotronElement.title
+			}
 		}
 
-		if (frameCount >= frames[(frames.length - 1)]) {
-			countUp = false;
-		} else if (frameCount <= 0) {
-			countUp = true;
-		}
+		var jumbotronTimer = setInterval(jumbotronTypewriter, typeInverval);
 
-		if ((frameCount >= frames[frameIndex + 1]) && (advanceRight == true)) {
-			frameIndex += 2
-		} else if ((frameCount <= frames[frameIndex - 1]) && (advanceRight == false)) {
-			frameIndex -= 2
-		}
+		function jumbotronTypewriter() 
+		{ 
+			if (frameCount < numSpins)
+			{
+				switch (jumbotronElement.innerHTML) {
+					case '|':
+						jumbotronElement.innerHTML = '/';
+						break;
+					case '/':
+						jumbotronElement.innerHTML = '—';
+						break;
+					case '—':
+						jumbotronElement.innerHTML = '\\';
+						break;
+					case '\\':
+						jumbotronElement.innerHTML = '|';
+						document.title += '.';
+						frameCount++;
+						break;
+				}
+			}
+			else
+			{
+				if (frameCount == numSpins)
+				{
+					jumbotronElement.innerHTML = '';
+					document.title = '';
+				}
 
-		frameCount += countUp ? 1 : -1
-	}, 100);
-};
+				if (jumbotronElement.innerHTML.length < jumboTxt.length) 
+				{
+					jumbotronElement.innerHTML += jumboTxt.charAt(frameCount - numSpins);
+				}
+
+				if (document.title.length < titleTxt.length)
+				{
+					var stringToAdd = titleTxt.charAt((frameCount - numSpins) + numSpaces);
+
+					if (stringToAdd == ' ') 
+					{
+						stringToAdd += titleTxt.charAt((frameCount - numSpins) + numSpaces + 1);
+						numSpaces++;
+					}
+					document.title += stringToAdd;
+				}
+
+				if ((jumbotronElement.innerHTML.length >= jumboTxt.length) && 
+					(document.title.length >= titleTxt.length))
+				{
+					clearInterval(jumbotronTimer);
+				}
+				frameCount++;
+			}
+		}
+	}
+}
+
+const commonScript = new common();
 
 if (document.readyState === "complete" ||
 	(document.readyState !== "loading" && !document.documentElement.doScroll)) 
 {
-	readyFunction();
+	commonScript.readyFunction();
 } 
 else 
 {
-	document.addEventListener("DOMContentLoaded", readyFunction);
+	document.addEventListener("DOMContentLoaded", commonScript.readyFunction());
 }
