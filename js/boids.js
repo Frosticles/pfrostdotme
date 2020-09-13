53b1fdc29843f136ccc3aef9f4ddcddea9e619cc
Peter Frost <frosticless@hotmail.com>
2020-09-13 01:16:07 +0100

    Loads of performance optimisations, reset boids to center if out of bounds, add minimum speed, add maximum angle variation speed, add some randomness to bird speed limits with a random seed.

--- js/boids.js
+++ js/boids.js
@@ -12,12 +12,19 @@ let visualRange;
 let borderMargin;
 let speedLimit;
 let minDistance;
-const centeringFactor = 0.005;
-const turnBias = 0.01;
-const initVelocity = 0.5;
-const numBoids = 100;
-const avoidFactor = 0.03;
-const vMatchingFactor = 0.02;
+const initVelocity = 1;
+const numBoids = 500;
+const centeringFactor = 0.4;
+const turnBias = 0.25;
+const avoidFactor = 0.2;
+const vMatchingFactor = 0.05;
+const maxAngleChange = 0.3;
+const minSpeedFactor = 0.7;
+const speedLimitFactor = 0.4;
+const speedRandomness = 0.2;
+const marginFactor = 4;
+const visualRangeFactor = 10;
+const minDistanceFactor = 1.5;
 
 
 
@@ -26,11 +33,14 @@ class boid
     currentX = 0;
     currentY = 0;
     currentZ = 0;
+    currentAngle = 0;
     deltaX = 0;
     deltaY = 0;
     deltaZ = 0;
     index = 0;
     colour = '';
+    speedLimit = 0;
+    seed = 0;
 }
 
 
@@ -80,6 +90,7 @@ function drawBoids()
         boidArray[i].deltaY = (Math.random() * initVelocity) - (2 * initVelocity);
         boidArray[i].index = i;
         boidArray[i].colour = 'white';
+        boidArray[i].seed = Math.random();
 
         drawIndividualBoid(i, boidArray[i].colour);
     }
@@ -88,53 +99,58 @@ function drawBoids()
 }
 
 
-function getDistance(boid1, boid2)
-{
-    const aSquared = (boid1.currentX - boid2.currentX) ** 2;
-    const bSquared = (boid1.currentY - boid2.currentY) ** 2;
-    const cSquared = aSquared + bSquared;
-
-    return Math.sqrt(cSquared);
-}
 
 
 
-function applyFlockingForces(currentBoid) 
+function applyFlockingForces(currentBoid, boidSize, frameTime) 
 {
     let numNeighbors = 0;
-
     let localCenterX = 0;
     let localCenterY = 0;
     let avoidNudgeX = 0;
     let avoidNudgeY = 0;
     let avgdeltaX = 0;
     let avgdeltaY = 0;
+
+    if (boidGetBounds(currentBoid, boidSize).inBounds == false)
+    {
+        return;
+    }
   
     for (let i = 0; i < numBoids; i++)
     {
         if (i !== currentBoid.index) 
-        {
-            const distance = getDistance(currentBoid, boidArray[i])
+        {            
+            const a = (boidArray[i].currentX - currentBoid.currentX);
+            const b = (currentBoid.currentY - boidArray[i].currentY);
+
+            // This is clearly a rubbish way to calculate distance
+            // but it works well enough, and it's fast.
+            const distance = Math.abs(a) + Math.abs(b);
             
             if (distance < visualRange) 
             {
+                if (Math.abs(currentBoid.currentAngle + Math.atan2(b, a)) > 1.5)
+                {
+                    continue;
+                }
+
                 localCenterX += boidArray[i].currentX;
                 localCenterY += boidArray[i].currentY;
                 avgdeltaX += boidArray[i].deltaX;
                 avgdeltaY += boidArray[i].deltaY;
                 numNeighbors += 1;
+
+                if (distance < minDistance)
+                {
+                    avoidNudgeX += (currentBoid.currentX - boidArray[i].currentX) * (minDistance - distance);
+                    avoidNudgeY += (currentBoid.currentY - boidArray[i].currentY) * (minDistance - distance);
+                }
             }
 
-            if (distance < minDistance)
-            {
-                avoidNudgeX += currentBoid.currentX - boidArray[i].currentX;
-                avoidNudgeY += currentBoid.currentY - boidArray[i].currentY;
-            }
+
         }
     }
-
-    currentBoid.deltaX += avoidNudgeX * avoidFactor;
-    currentBoid.deltaY += avoidNudgeY * avoidFactor;
   
     if (numNeighbors > 0) 
     {
@@ -143,19 +159,31 @@ function applyFlockingForces(currentBoid)
         avgdeltaX /= numNeighbors;
         avgdeltaY /= numNeighbors;
     
-        currentBoid.deltaX += (localCenterX - currentBoid.currentX) * centeringFactor;
-        currentBoid.deltaY += (localCenterY - currentBoid.currentY) * centeringFactor;
-        currentBoid.deltaX += (avgdeltaX - currentBoid.deltaX) * vMatchingFactor;
-        currentBoid.deltaY += (avgdeltaY - currentBoid.deltaY) * vMatchingFactor;
-        drawIndividualBoid(currentBoid.index, 'green');
+        currentBoid.deltaX += (localCenterX - currentBoid.currentX) * centeringFactor * frameTime;
+        currentBoid.deltaY += (localCenterY - currentBoid.currentY) * centeringFactor * frameTime;
+        currentBoid.deltaX += (avgdeltaX - currentBoid.deltaX) * vMatchingFactor * frameTime;
+        currentBoid.deltaY += (avgdeltaY - currentBoid.deltaY) * vMatchingFactor * frameTime;
+        currentBoid.deltaX += avoidNudgeX * avoidFactor * frameTime;
+        currentBoid.deltaY += avoidNudgeY * avoidFactor * frameTime;
+        //currentBoid.speedLimit -= (numBoids / 4) - numNeighbors;
+
+
+        if (avoidNudgeX != 0)
+        {
+            drawIndividualBoid(currentBoid.index, 'purple');
+        }
+        else
+        {
+            drawIndividualBoid(currentBoid.index, 'green');
+        }
     }
     else
     {
         // This will cause boids with no neighbour to head towards the centre but with
         // a bit of random walk introduced by the other boid.
         otherBoid = boidArray[numBoids - currentBoid.index - 1];
-        currentBoid.deltaX += (((boidsDiv.boundingRect.width / 2) - currentBoid.currentX) - otherBoid.deltaX) * centeringFactor;
-        currentBoid.deltaY += (((boidsDiv.boundingRect.height / 2) - currentBoid.currentY) - otherBoid.deltaY) * centeringFactor;
+        currentBoid.deltaX += otherBoid.deltaX * centeringFactor * frameTime;
+        currentBoid.deltaY += otherBoid.deltaY * centeringFactor * frameTime;
         drawIndividualBoid(currentBoid.index, 'red');
     }
 }
@@ -163,21 +191,37 @@ function applyFlockingForces(currentBoid)
 
 
 
-function limitSpeed(currentBoid) 
+function limitVelocity(currentBoid, frameTime) 
 {
     const speed = Math.sqrt((currentBoid.deltaX ** 2) + (currentBoid.deltaY ** 2));
+    const angle = Math.atan2(currentBoid.deltaY, currentBoid.deltaX);
+    const angleDiff = angle - currentBoid.currentAngle;
 
-    if (speed > speedLimit) 
+    if (Math.abs(angleDiff) > maxAngleChange)
     {
-        currentBoid.deltaX = (currentBoid.deltaX / speed) * speedLimit;
-        currentBoid.deltaY = (currentBoid.deltaY / speed) * speedLimit;
+        let angleChange = (angleDiff > 0) ? maxAngleChange : -maxAngleChange
+        let newAngle = currentBoid.currentAngle + angleChange;
+
+        currentBoid.deltaX = Math.cos(newAngle) * speed;
+        currentBoid.deltaY = Math.sin(newAngle) * speed;
     }
-  }
+
+    if (speed > currentBoid.speedLimit) 
+    {
+        currentBoid.deltaX = (currentBoid.deltaX / speed) * currentBoid.speedLimit;
+        currentBoid.deltaY = (currentBoid.deltaY / speed) * currentBoid.speedLimit;
+    }
+    else if (speed < (currentBoid.speedLimit * minSpeedFactor))
+    {
+        currentBoid.deltaX = (currentBoid.deltaX / speed) * (currentBoid.speedLimit * minSpeedFactor);
+        currentBoid.deltaY = (currentBoid.deltaY / speed) * (currentBoid.speedLimit * minSpeedFactor);
+    }
+}
 
 
 
 
-function keepWithinBounds(currentBoid, boidSize)
+function boidGetBounds(currentBoid, boidSize)
 {
     const width = boidsDiv.boundingRect.width - boidSize;
     const height = boidsDiv.boundingRect.height - boidSize;
@@ -185,30 +229,54 @@ function keepWithinBounds(currentBoid, boidSize)
     const tooFarRight = (currentBoid.currentX > (width - borderMargin));
     const tooFarUp = (currentBoid.currentY < borderMargin);
     const tooFarDown = (currentBoid.currentY > (height - borderMargin));
-    
-    if ((tooFarLeft) || (tooFarRight))
+    let inBounds = true;
+
+    if (tooFarLeft || tooFarRight || tooFarUp || tooFarDown)
     {
-        const edgePos = (tooFarLeft) ? borderMargin : (width - borderMargin);
+        inBounds = false;
+    }
+    
+    return {
+        inBounds: inBounds,
+        tooFarLeft: tooFarLeft,
+        tooFarRight: tooFarRight,
+        tooFarUp: tooFarUp,
+        tooFarDown: tooFarDown,
+    };
+}
+
+
+
+function keepWithinBounds(currentBoid, boidSize, frameTime)
+{
+    const width = boidsDiv.boundingRect.width - boidSize;
+    const height = boidsDiv.boundingRect.height - boidSize;
+    const bounds = boidGetBounds(currentBoid, boidSize);
+
+    
+    if ((bounds.tooFarLeft) || (bounds.tooFarRight))
+    {
+        const edgePos = (bounds.tooFarLeft) ? borderMargin : (width - borderMargin);
         const changeFactor = ((edgePos - currentBoid.currentX) / borderMargin) ** 2;
         const vChange = Math.abs(currentBoid.deltaX * changeFactor) + turnBias;
 
-        currentBoid.deltaX += (tooFarLeft) ? vChange : -vChange;
+        currentBoid.deltaX += (bounds.tooFarLeft) ? vChange : -vChange;
 
-        if ((!tooFarUp) && (!tooFarDown))
+        if ((!bounds.tooFarUp) && (!bounds.tooFarDown))
         {
             currentBoid.deltaY += (currentBoid.deltaY > 0) ? vChange : -vChange;
         }
     }
 
-    if ((tooFarUp) || (tooFarDown))
+    if ((bounds.tooFarUp) || (bounds.tooFarDown))
     {
-        const edgePos = (tooFarUp) ? borderMargin : (height - borderMargin);
+        const edgePos = (bounds.tooFarUp) ? borderMargin : (height - borderMargin);
         const changeFactor = ((edgePos - currentBoid.currentY) / borderMargin) ** 2;
         const vChange = Math.abs(currentBoid.deltaY * changeFactor) + turnBias;
 
-        currentBoid.deltaY += (tooFarUp) ? vChange : -vChange;
+        currentBoid.deltaY += (bounds.tooFarUp) ? vChange : -vChange;
 
-        if ((!tooFarLeft) && (!tooFarRight))
+        if ((!bounds.tooFarLeft) && (!bounds.tooFarRight))
         {
             currentBoid.deltaX += (currentBoid.deltaX > 0) ? vChange : -vChange;
         }
@@ -217,11 +285,13 @@ function keepWithinBounds(currentBoid, boidSize)
     if ((currentBoid.currentX < 0) || (currentBoid.currentX > width))
     {
         console.log("Exceeded width");
+        currentBoid.currentX = width / 2;
     }
 
     if ((currentBoid.currentY < 0) || (currentBoid.currentY > height))
     {
         console.log("Exceeded height");
+        currentBoid.currentY = height / 2;
     }
 }
 
@@ -241,24 +311,26 @@ function stepBoids(timestamp)
     boidsDiv.boundingRect = boidsDiv.getBoundingClientRect();
 
     const boidSize = Math.min((boidsDiv.boundingRect.width * 0.02), 20);
-    visualRange = boidSize * 10;
-    borderMargin = boidSize * 6;
-    speedLimit = boidSize * 0.6;
-    minDistance = boidSize * 2;
+    visualRange = boidSize * visualRangeFactor;
+    borderMargin = boidSize * marginFactor;
+    minDistance = boidSize * minDistanceFactor;
 
     for (let i = 0; i < numBoids; i++) 
     {
-        applyFlockingForces(boidArray[i]);
-        keepWithinBounds(boidArray[i], boidSize);
-        limitSpeed(boidArray[i]);
+        boidArray[i].speedLimit = (boidSize * speedLimitFactor) + (boidArray[i].seed * speedRandomness);
+
+        applyFlockingForces(boidArray[i], boidSize, frameTime);
+        limitVelocity(boidArray[i], frameTime);
+        keepWithinBounds(boidArray[i], boidSize, frameTime);
 
         boidArray[i].currentX += boidArray[i].deltaX;
         boidArray[i].currentY += boidArray[i].deltaY;
+        boidArray[i].currentAngle = Math.atan2(boidArray[i].deltaY, boidArray[i].deltaX);
 
         canvasArray[i].style.transform = `translate3d(${boidArray[i].currentX}px,
                                                       ${boidArray[i].currentY}px, 
                                                       0px)
-                                          rotate(${Math.atan2(boidArray[i].deltaY, boidArray[i].deltaX)}rad)`;
+                                          rotate(${boidArray[i].currentAngle}rad)`;
     }
 
     window.requestAnimationFrame(stepBoids);
